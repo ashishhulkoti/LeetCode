cead911955441e9aead37b8188a743840823c54c
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-01-29 15:17:36 -0600

    Time: 9 ms (8.3%), Space: 18.2 MB (16.04%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,38 +1,25 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        p1,p2=0,0
-        count=1
-        m,n=len(nums1),len(nums2)
-        even=False
-        first,second=0,0
-        while p1<= m-1 and p2<=n-1 and count<=(m+n)//2+1:
-            if nums1[p1]<nums2[p2]:
-                # nums.append(nums1[p1])
-                first=second
-                second=nums1[p1]
-                p1+=1
+        size=len(nums1)+len(nums2)
+        nums3=[]
+        mid=size//2
+        count,i,j=0,0,0
+        while i+j <= mid and nums1 and nums2:
+            if nums1[0]<nums2[0]:
+                nums3.append(nums1[0])
+                # print("Popping1 : ",nums1[0])
+                nums1.pop(0)
+                i+=1
             else:
-                # nums.append(nums2[p2])
-                first=second
-                second=nums2[p2]
-                p2+=1
-            count+=1
-        print(p1,p2,count)
-        if count!=(m+n)//2+2:
-            if p1==m:
-                while count<=(m+n)//2+1:
-                    first=second
-                    second=nums2[p2]
-                    p2+=1
-                    count+=1
-            else:
-                while count<=(m+n)//2+1:
-                    first=second
-                    second=nums1[p1]
-                    p1+=1
-                    count+=1
-        if ((m+n)%2 == 0):
-            return (second+first)/2
-        else:
-            return second
-
+                nums3.append(nums2[0])
+                # print("Popping2 : ",nums2[0])
+                nums2.pop(0)
+                j+=1
+        if 0==len(nums1):
+            nums3.extend(nums2)
+        if 0==len(nums2):
+            nums3.extend(nums1)
+        # print(nums3,mid)
+        if size%2==0:
+            return (nums3[mid]+nums3[mid-1])/2
+        return nums3[mid]
