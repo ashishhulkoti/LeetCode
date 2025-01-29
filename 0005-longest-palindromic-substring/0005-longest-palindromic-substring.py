459bab33005d00c9c0fbd703e29e92f43dfc112f
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-01-29 16:42:10 -0600

    Time: 3435 ms (18.74%), Space: 18 MB (40.04%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,43 +1,28 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        longest=""
-        first_part,second_part="",""
-        longest_length=0
-        k=0
-        str_size=len(s)
+        # dp=[[0]*len(s) for i in range(len(s))]
+        s_size=len(s)
+        longest_length=1
+        longest=s[0]
+        def check_palindrome(m,n):
+            while m<=n:
+                if s[m]!=s[n]:
+                    return False
+                m+=1
+                n-=1
+            return True
         
+        if s_size==1:
+            return s
+        elif s_size==2:
+            if s[0]==s[1]:
+                return s
+            else:
+                return s[0]
         
-
-        while (k+longest_length) < str_size:
-            temp=longest_length
-            while (k+temp) < str_size:
-                i=k
-                j=k+temp
-                dum_length=0
-                dum=""
-                while i<j:
-                    if s[i]==s[j]:
-                        first_part=first_part + s[i]
-                        second_part=s[j] + second_part
-                    else:
-                        break
-                    i+=1
-                    j-=1
-
-                if i==j:
-                    dum=first_part+s[i]+second_part
-                    dum_length=len(dum)
-                elif i>j:
-                    dum=first_part+second_part
-                    dum_length=len(dum)
-                if dum_length>longest_length:
-                    longest_length=dum_length
-                    longest=dum
-                temp+=1
-                first_part=""
-                second_part=""
-            k+=1
+        for i in range(s_size):
+            for j in range(i,s_size):
+                if j-i+1 > longest_length and check_palindrome(i,j):
+                    longest_length = j-i+1
+                    longest=s[i:j+1]
         return longest
-        
-            
-                
