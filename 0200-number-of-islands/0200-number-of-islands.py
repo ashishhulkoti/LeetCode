0f361e42cc8050eb9deec3d2b7fd27130749a2c1
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-02-02 20:48:16 -0600

    Time: 244 ms (66.27%), Space: 20 MB (85.16%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,10 +1,10 @@
 class Solution:
     
     def grid_dfs(self,grid,k,l):
-        if k<0 or l<0 or l>=len(grid[0]) or k>=len(grid):
+        if k<0 or l<0 or l>=len(grid[0]) or k>=len(grid) or grid[k][l]!="1":
             return
-        if grid[k][l]!="1":
-            return
+        # if grid[k][l]!="1":
+        #     return
         grid[k][l]="0"
         self.grid_dfs(grid,k-1,l)
         self.grid_dfs(grid,k,l-1)
