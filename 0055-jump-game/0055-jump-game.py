f3a9f47cbebc909e41571fb0fdadedcf6330ec6e
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-02-05 20:10:47 -0600

    Time: 12 ms (83.66%), Space: 18.6 MB (48.7%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -4,9 +4,11 @@ class Solution:
             return True
         dest=len(nums)-1
         jump_position=dest-1
-        while jump_position>=0:
+        while jump_position>=0 and dest!=0:
             if (jump_position+nums[jump_position])>=dest:
                 dest=jump_position
+                # if jump_position==0:
+                #     return True            
             jump_position-=1
         if dest==0:
             return True
