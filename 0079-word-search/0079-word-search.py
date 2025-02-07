fbcdbd48a8ff294a12845f6a9b83f412369b6583
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-02-07 17:55:45 -0600

    Time: 1672 ms (93.12%), Space: 18.1 MB (19.1%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -3,11 +3,11 @@ class Solution:
         # print(i,j,k)
         if i<0 or j<0 or i>=len(board) or j>=len(board[0]) or board[i][j]!=word_array[k]:
             return False
-        if k==len(word_array)-1:
+        if k==0:
             return True
         tmp=board[i][j]
         board[i][j]=""
-        if self.exist_helper(board,i+1,j,word_array,k+1) or self.exist_helper(board,i,j+1,word_array,k+1) or self.exist_helper(board,i-1,j,word_array,k+1) or self.exist_helper(board,i,j-1,word_array,k+1):
+        if self.exist_helper(board,i+1,j,word_array,k-1) or self.exist_helper(board,i,j+1,word_array,k-1) or self.exist_helper(board,i-1,j,word_array,k-1) or self.exist_helper(board,i,j-1,word_array,k-1):
             return True
         else:
             board[i][j]=tmp
@@ -20,9 +20,10 @@ class Solution:
         if m*n < len(word):
             return False
         word_array=list(word)
-        for i in range(m):
-            for j in range(n):
-                if board[i][j]==word_array[0] and self.exist_helper(board,i,j,word_array,0):
+        lw=len(word_array)-1
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                if board[i][j]==word_array[lw] and self.exist_helper(board,i,j,word_array,lw):
                     return True
         return False
                 
