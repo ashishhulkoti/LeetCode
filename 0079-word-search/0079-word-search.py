0b32d54e885721a687ea1f5db3af40ccbd4fce6e
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-02-07 17:19:41 -0600

    Time: 3501 ms (75.9%), Space: 17.9 MB (35.65%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,28 @@
+class Solution:
+    def exist_helper(self,board,i,j,word_array,k):
+        # print(i,j,k)
+        if i<0 or j<0 or i>=len(board) or j>=len(board[0]) or board[i][j]!=word_array[k]:
+            return False
+        if k==len(word_array)-1:
+            return True
+        tmp=board[i][j]
+        board[i][j]=""
+        if self.exist_helper(board,i+1,j,word_array,k+1) or self.exist_helper(board,i,j+1,word_array,k+1) or self.exist_helper(board,i-1,j,word_array,k+1) or self.exist_helper(board,i,j-1,word_array,k+1):
+            return True
+        else:
+            board[i][j]=tmp
+            return False
+
+
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m=len(board)
+        n=len(board[0])
+        if m*n < len(word):
+            return False
+        word_array=list(word)
+        for i in range(m):
+            for j in range(n):
+                if board[i][j]==word_array[0] and self.exist_helper(board,i,j,word_array,0):
+                    return True
+        return False
+                
