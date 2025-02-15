3313788fdc0c142bf0802275902b58afd2838079
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-02-15 15:57:43 -0600

    Time: 44 ms (37.37%), Space: 31.7 MB (67.68%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.py
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.py
@@ -3,22 +3,23 @@ class ProductOfNumbers:
     def __init__(self):
         self.prod_list=[]
         self.last=1
+        self.size=0
     def add(self, num: int) -> None:    
         if num==0:
             self.prod_list=[]
             self.last=1
+            self.size=0
         else:
             self.last=self.last*num
             self.prod_list.append(self.last)
+            self.size+=1
 
     def getProduct(self, k: int) -> int:
-        # print(self.prod_dict)
-        size=len(self.prod_list)
-        if k>size:
+        if k>self.size:
             return 0
-        elif k==size:
+        elif k==self.size:
             return self.last
-        return self.last//self.prod_list[size-k-1]
+        return self.last//self.prod_list[self.size-k-1]
 
             
 
