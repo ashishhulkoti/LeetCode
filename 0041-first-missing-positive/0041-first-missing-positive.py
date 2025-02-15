92125647110fc9b5ef615e96952fb42e817e27ea
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-02-15 17:19:42 -0600

    Time: 54 ms (33.74%), Space: 28.9 MB (67.69%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -5,15 +5,20 @@ class Solution:
         checker=1
         while i<len(nums) and nums[i]<=0:
             i+=1
-        while i<(len(nums)-1):
-            if nums[i]==nums[i+1]:
+        if i==len(nums) or nums[i]!=1:
+            return 1
+        i+=1
+        checker+=1
+        prev=i-1
+        while i<len(nums):
+            if nums[i]==nums[i-1]:
                 i+=1
                 continue
-            if nums[i]==checker:
+            
+            if nums[i]==nums[prev]+1:
                 checker+=1
+                prev=i
                 i+=1
             else:
                 return checker
-        if i==len(nums) or nums[i]!=checker:
-            return checker
-        return checker+1
+        return checker
