b174ce272a85823a536221e7c29624b440c8766f
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-02-15 15:43:13 -0600

    Time: 40 ms (47.75%), Space: 33.8 MB (10.19%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.py
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.py
@@ -5,14 +5,15 @@ class ProductOfNumbers:
         self.last=-1
         self.zeroes=-1
     def add(self, num: int) -> None:
+        self.last+=1        
         if num==0:
-            self.prod_dict[self.last+1]=self.prod_dict[self.last]
-            self.zeroes=self.last+1
+            self.prod_dict[self.last]=1
+            self.zeroes=self.last
         else:
-            self.prod_dict[self.last+1]=self.prod_dict[self.last]*num
-        self.last+=1
+            self.prod_dict[self.last]=self.prod_dict[self.last-1]*num
 
     def getProduct(self, k: int) -> int:
+        # print(self.prod_dict)
         if (self.last-k+1)>self.zeroes:
             return self.prod_dict[self.last]//self.prod_dict[self.last-k]
         return 0
