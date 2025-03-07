bafed5ab9ef6f317a3458e41c0071a3087da0aab
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-03-07 17:15:17 -0600

    Time: 0 ms (100%), Space: 19.9 MB (30.73%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        
+
+        def validHelper(node,parentVal,rootVal,left):
+            if not node:
+                return True
+            if left:
+                if node.val >= parentVal or node.val<=rootVal:
+                    print("here 1", node.val, parentVal, rootVal)
+                    return False
+                if not validHelper(node.left,node.val,rootVal,True) or not validHelper(node.right,node.val,parentVal,False):
+                    return False
+            else:
+                if node.val <= parentVal or node.val>=rootVal:
+                    print("here 2", node.val, parentVal, rootVal)
+                    return False
+                if not validHelper(node.left,node.val,parentVal,True) or not validHelper(node.right,node.val,rootVal,False):
+                    return False
+            return True
+
+        
+        return validHelper(root.left,root.val,float("-inf"),True) and validHelper(root.right,root.val,float("inf"),False) 
+
+             
+            
