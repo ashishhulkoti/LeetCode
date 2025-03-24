e7131172d74e3ba2c3b07a5d74417e6ae4175403
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-03-23 20:00:39 -0500

    Time: 88 ms (97.3%), Space: 54.5 MB (97.83%) - LeetHub

---
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
+        topPath=0
+        bottomPath=0
+        # nodesBeforeStart=0
+
+        def treeTraversal(node) -> int:
+            nonlocal topPath
+            nonlocal bottomPath
+            if not node:
+                return 0
+            
+            if node.val==start:
+                bottomPath = max(treeTraversal(node.left),treeTraversal(node.right))
+                return -1
+            
+            left = treeTraversal(node.left)
+            right = treeTraversal(node.right)
+            if right < 0:
+                if right*(-1)+left > topPath:
+                    topPath=right*(-1)+left
+                return right-1
+            elif left < 0:
+                if left*(-1)+right > topPath:
+                    topPath=left*(-1)+right
+                return left-1
+            return 1 + max(left,right)
+
+        treeTraversal(root)
+        return max(topPath,bottomPath)
+
+
+# 16 0 0
+# 17 1 0
+# 6 0 0
+# 13 0 0
+# 20 -1 1
+# returning left:  -2
+# 4 -2 0
+# 15 2 None
+
+            
