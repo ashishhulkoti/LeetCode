84b8105ae1067f5461a851b6517439765cc18624
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-04-07 22:08:59 -0500

    Time: 1261 ms (34.92%), Space: 392.2 MB (7.21%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,15 +1,18 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        flag=False
         @cache
         def sumHelper(i,currSum):
             nonlocal nums
+            nonlocal flag
             # print (currSum)
             if currSum==0:
                 return True
             if currSum < 0 or i < 0 :
                 return False
             
-            if sumHelper(i-1,currSum) or sumHelper(i-1,currSum-nums[i]):
+            if not flag and sumHelper(i-1,currSum) or sumHelper(i-1,currSum-nums[i]):
+                flag=True
                 return True
             return False
         
