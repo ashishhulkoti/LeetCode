db7fc516d57c788af5ae3e83aa6cc291bd98ab47
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-04-16 23:34:32 -0500

    Time: 37 ms (91.49%), Space: 37.2 MB (38.89%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.py
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.py
@@ -1,20 +1,26 @@
 class Solution:
     def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
-        times=[[0,0]]
-        for i in range(len(startTime)):
-            times.append([startTime[i],endTime[i]])
-        times.append([eventTime,eventTime])
+        # times=[[0,0]]
+        # for i in range(len(startTime)):
+        #     times.append([startTime[i],endTime[i]])
+        # times.append([eventTime,eventTime])
         # times.sort(key = lambda x : x[0])
-        diff=[]
-        for i in range(len(times)-1):
-            diff.append(times[i+1][0]-times[i][1])
+        # diff=[]
+        # for i in range(len(times)-1):
+        #     diff.append(times[i+1][0]-times[i][1])
+        startTime.insert(0,0)
+        startTime.append(eventTime)
+        endTime.insert(0,0)
+        endTime.append(eventTime)
         l,h=0,k
         maxSum=0
         currSum=0
         # print(diff)
-        for i in range(len(diff)):
+        diff=[]
+        for i in range(len(startTime)-1):
+            diff.append(startTime[i+1] - endTime[i])
             if i>h:
-                currSum = currSum - diff[l]
+                currSum = currSum - (diff[l])
                 l+=1
             currSum += diff[i]
             if currSum > maxSum:
