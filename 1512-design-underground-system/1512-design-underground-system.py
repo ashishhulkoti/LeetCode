2f44577b08b9e186b50ef92a50ff9652427e5aed
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-05-04 16:03:18 -0500

    Time: 39 ms (11.17%), Space: 33.2 MB (6.39%) - LeetHub

---
+++ 1512-design-underground-system/1512-design-underground-system.py
@@ -0,0 +1,29 @@
+class UndergroundSystem:
+
+    def __init__(self):
+        self.stationCheckins = defaultdict(list)
+        self.stationCheckouts = defaultdict(list)
+        self.stationAverageTimes = {}
+
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        self.stationCheckins[id].append((t,stationName))
+
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        self.stationCheckouts[id].append((t,stationName))
+        stations = (self.stationCheckins[id][-1][1],stationName)
+        entrytime = self.stationCheckins[id][-1][0]
+        if stations in self.stationAverageTimes:
+            self.stationAverageTimes[stations][0]+= (t-entrytime)
+            self.stationAverageTimes[stations][1] += 1
+        else:
+            self.stationAverageTimes[stations] = [t-entrytime, 1]
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        return self.stationAverageTimes[(startStation,endStation)][0] / self.stationAverageTimes[(startStation,endStation)][1]
+
+
+# Your UndergroundSystem object will be instantiated and called as such:
+# obj = UndergroundSystem()
+# obj.checkIn(id,stationName,t)
+# obj.checkOut(id,stationName,t)
+# param_3 = obj.getAverageTime(startStation,endStation)
