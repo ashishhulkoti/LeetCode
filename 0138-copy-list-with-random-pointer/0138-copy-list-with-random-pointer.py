cccaa2f24ef83caf601d2d0fa67e3b1f0d99582d
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-05-05 14:03:46 -0500

    Time: 44 ms (40.11%), Space: 18.5 MB (59.89%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,31 +9,18 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if not head:
-            return head
-        node_dict={}
-        headn=Node(head.val)
-        node_dict[head]=headn
+        node_dict = {}
+        node_dict[None] = None
+        def dfs(node):
+            if not node:
+                return None
+            
+            if node not in node_dict:
+                node_dict[node] = Node(node.val)
+            
+            node_dict[node].next = dfs(node.next)
+            node_dict[node].random = node_dict[node.random]
 
-        # prev=None
-        curr=head
-        while curr:
-            node=None
-            if curr.next:
-                if curr.next in node_dict:
-                    node_dict[curr].next=node_dict[curr.next]
-                else:
-                    nnode=Node(curr.next.val)
-                    node_dict[curr].next=nnode
-                    node_dict[curr.next]=nnode
-            if curr.random:
-                if curr.random in node_dict:
-                    node_dict[curr].random=node_dict[curr.random]
-                else:
-                    nnode=Node(curr.random.val)
-                    node_dict[curr].random=nnode
-                    node_dict[curr.random]=nnode
-            curr=curr.next
-        return headn
-
-                
+            return node_dict[node]
+        
+        return dfs(head)
