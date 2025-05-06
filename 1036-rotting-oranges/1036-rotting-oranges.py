5a1a9de21d8247d6030524c6dbb2ceeb4c72f78d
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-05-06 00:32:00 -0500

    Time: 11 ms (6.61%), Space: 18 MB (6.46%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -2,31 +2,36 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         dp = [[float("inf")] * len(grid[0]) for _ in range(len(grid))]
 
+        original_rotten = set()
+
+        def bfsHelper(currQ):
+            nonlocal dp, grid, original_rotten
+            t = 0
+            while currQ:
+                orngr, orngc, orngt = currQ.pop(0)
+                if grid[orngr][orngc] == 0:
+                    continue
+                if grid[orngr][orngc] == 2 and (orngr, orngc) not in original_rotten:
+                    original_rotten.add((orngr, orngc))
+                    bfsHelper([(orngr,orngc,0)])    
+
+                if orngt < dp[orngr][orngc]:
+                    dp[orngr][orngc] = orngt
+                else:
+                    continue
+                for r,c in [(0,1),(1,0),(-1,0),(0,-1)]:
+                    if 0 <= orngr+ r < len(grid) and 0 <= orngc+ c < len(grid[0]):
+                            currQ.append((orngr + r,orngc + c, orngt + 1))
+
+
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 
                 if grid[i][j] == 2:
-                    currQ = []
-                    currQ.append((i,j,0))
-                    t = 0
-
-                    while currQ:
-                        orngr, orngc, orngt = currQ.pop(0)
-                        if grid[orngr][orngc] == 0:
-                            continue
-                        # if grid[orngr][orngc] == 2:
-                        #     while currQ:
-                        #         orngr, orngc, orngt = currQ.pop(0)
-                        #         if orngt < dp[orngr][orngc]:
-                        #             dp[orngr][orngc] = orngt
-                        #     break
-                        if orngt < dp[orngr][orngc]:
-                            dp[orngr][orngc] = orngt
-                        else:
-                            continue
-                        for r,c in [(0,1),(1,0),(-1,0),(0,-1)]:
-                            if 0 <= orngr+ r < len(grid) and 0 <= orngc+ c < len(grid[0]):
-                                    currQ.append((orngr + r,orngc + c, orngt + 1))
+                    if (i,j) in original_rotten:
+                        continue
+                    original_rotten.add((i,j))
+                    bfsHelper([(i,j,0)])
         maxT=0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
@@ -36,7 +41,3 @@ class Solution:
                     if dp[i][j]>maxT:
                         maxT=dp[i][j]
         return maxT
-                    
-                                
-                                                    
-
