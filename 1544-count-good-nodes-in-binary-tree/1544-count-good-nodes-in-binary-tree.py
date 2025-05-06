3f3175b7d3c53685f0ff1f4bc4554d56431a42fb
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-05-06 11:38:58 -0500

    Time: 148 ms (42.93%), Space: 31.9 MB (87.06%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -1,4 +1,3 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
