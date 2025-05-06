5d0ee92e1002a4db08c5c4c409bb82e3ca5d0e5d
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-05-06 00:09:43 -0500

    Time: 18 ms (6.61%), Space: 18 MB (6.46%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -0,0 +1,37 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        dp = [[float("inf")] * len(grid[0]) for _ in range(len(grid))]
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                
+                if grid[i][j] == 2:
+                    currQ = []
+                    currQ.append((i,j,0))
+                    t = 0
+
+                    while currQ:
+                        orngr, orngc, orngt = currQ.pop(0)
+                        if grid[orngr][orngc] == 0:
+                            continue
+
+                        if orngt < dp[orngr][orngc]:
+                            dp[orngr][orngc] = orngt
+                        else:
+                            continue
+                        for r,c in [(0,1),(1,0),(-1,0),(0,-1)]:
+                            if 0 <= orngr+ r < len(grid) and 0 <= orngc+ c < len(grid[0]):
+                                    currQ.append((orngr + r,orngc + c, orngt + 1))
+        maxT=0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    if dp[i][j] == float("inf"):
+                        return -1
+                    if dp[i][j]>maxT:
+                        maxT=dp[i][j]
+        return maxT
+                    
+                                
+                                                    
+
