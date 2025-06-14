8f0983e40d7ef1a01a2976e3081d922bd3f1a046
ashishhulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-06-14 16:15:24 -0500

    Time: 91 ms (50.48%), Space: 57 MB (93.49%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -2,13 +2,19 @@ class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
         
 
-        dp = [-1] * len(questions)
-        def maxPoints(i):
-            if i >= len(questions):
-                return 0
-            if dp[i] != -1:
-                return dp[i]
-            dp[i] = max(questions[i][0] + maxPoints(i + questions[i][1] + 1), maxPoints(i+1))
-            return dp[i]
-        
-        return maxPoints(0)
+        dp = [0] * (len(questions)+1)
+        # def maxPoints(i):
+        #     if i >= len(questions):
+        #         return 0
+        #     if dp[i] != -1:
+        #         return dp[i]
+        #     dp[i] = max(questions[i][0] + maxPoints(i + questions[i][1] + 1), maxPoints(i+1))
+        #     return dp[i]
+
+        # return maxPoints(0)
+        for i in range(len(questions)-1,-1,-1):
+            takeIdx = min(i + 1 + questions[i][1], len(dp)-1)
+            dp[i] = max(questions[i][0] + dp[takeIdx], dp[i+1])
+            # if takeIdx <= len(questions) - 1:
+                # dp[i] = max(dp[i], dp[takeIdx] + questions[i][0])
+        return dp[0]
