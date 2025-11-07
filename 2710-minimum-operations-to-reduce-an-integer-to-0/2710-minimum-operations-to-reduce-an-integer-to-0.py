40a6b7852192a19055a574fded7d25a75ced7854
Ashish Hulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-11-06 20:22:13 -0600

    Time: 0 ms (100%), Space: 17.6 MB (81.1%) - LeetHub

--- 2710-minimum-operations-to-reduce-an-integer-to-0/2710-minimum-operations-to-reduce-an-integer-to-0.py
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/2710-minimum-operations-to-reduce-an-integer-to-0.py
@@ -3,10 +3,7 @@ class Solution:
     def minOperations(self, n: int) -> int:
         count = 0
         x = n
-        # print(math.log2(10))
-        # return 0
         while n!=0:
             n = abs(n - 2**(int(round(math.log2(n)))))
-            # print(n)
             count+=1
         return count
