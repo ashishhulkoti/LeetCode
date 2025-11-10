b6a9de60ba3488329fa9268176cbca31020ab84d
Ashish Hulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-11-10 14:29:47 -0600

    Time: 71 ms (12.54%), Space: 28.8 MB (89.12%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,9 +1,21 @@
 class Solution:
+    def swap(self, nums, i, j):
+        if nums[i] == nums[j]:
+            return False
+        nums[i] , nums[j] = nums[j], nums[i]
+        return True
     def firstMissingPositive(self, nums: List[int]) -> int:
-        nums_dict=Counter(nums)
-        last=1
-        for i in range(1,len(nums_dict)+1):
-            if i not in nums_dict:
-                return i
-            last+=1
-        return last
+        
+        i = 0
+        while i<len(nums):
+            if nums[i]>=1 and nums[i]<len(nums) and nums[i]!=(i+1):
+                if not self.swap(nums, i, nums[i]-1):
+                    i+=1
+            else:
+                i+=1
+
+        for i, number in enumerate(nums):
+            if number != (i+1):
+                return i+1
+        return len(nums) + 1
+            
