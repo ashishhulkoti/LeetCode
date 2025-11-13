a73836b56933db378a9db86a362cc08c1dcc3e1a
Ashish Hulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-11-13 12:57:38 -0600

    Time: 0 ms (100%), Space: 18 MB (8.26%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.py
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -1,20 +1,26 @@
-from collections import Counter
+import heapq
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        if len(s)==1:
-            return s
-        c_count=Counter(s)
-        if c_count.most_common()[0][1] > (len(s)+1)//2:
+        freq = defaultdict(int)
+        maxFreq = 0
+        for c in s:
+            freq[c] += 1
+            maxFreq = max(maxFreq, freq[c])
+        if maxFreq > (len(s)+1)//2:
             return ""
-        ans=[""]*len(s)
-        i=0
-        for c,n in c_count.most_common():
-            while n>0:
-                ans[i]=c
-                n-=1
-                i+=2
-                if i>=len(ans):
-                    i=1
-        return ''.join(ans)
+        ans = [""] * len(s)
+        i = 0
+        for char, cfreq in sorted(freq.items(), key = lambda x: x[1], reverse = True):
+            curFreq = cfreq
+            while curFreq > 0 and i<len(s):
+                ans[i] = char
+                curFreq -= 1
+                i += 2
+                if i >= len(s):
+                    i = 1
+        return "".join(ans)
 
-        
+
+
+
+
