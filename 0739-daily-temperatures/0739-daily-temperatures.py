ff97ebcdd3c4798213d92efa9a2b42cd430c2880
Ashish Hulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-11-13 12:29:02 -0600

    Time: 117 ms (45.8%), Space: 26.8 MB (93.33%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -6,8 +6,6 @@ class Solution:
         ans = [0] * len(temperatures)
 
         for i, currTemp in enumerate(temperatures):
-            if not stack or temperatures[stack[-1]] >= currTemp:
-                stack.append(i)
             while stack and temperatures[stack[-1]] < currTemp:
                 ans[stack[-1]] = i - stack[-1]
                 stack.pop()
