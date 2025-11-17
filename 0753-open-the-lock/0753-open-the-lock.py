a3ae0af387ae34f07eb4959149e70e5f0f1ed09f
Ashish Hulkoti <60284107+ashishhulkoti@users.noreply.github.com>
2025-11-16 22:18:45 -0600

    Time: 397 ms (53.82%), Space: 19.1 MB (82.66%) - LeetHub

--- 0753-open-the-lock/0753-open-the-lock.py
+++ 0753-open-the-lock/0753-open-the-lock.py
@@ -4,24 +4,23 @@ class Solution:
         que = deque()
         deadendSet = set(deadends)
         seen = set()
+        seen.add("0000")
         que.append(["0000", 0])
-
         while que:
             comb, count = que.popleft()
-            if comb in deadendSet or comb in seen:
+            if comb in deadendSet:
                 continue
-            seen.add(comb)
             if comb == target:
                 return count
             
             for i in range(4):
-                newCombB, newCombF = list(comb), list(comb)
-                newCombB[i] = str(abs(int(comb[i]) + 9)%10)
-                newCombF[i] = str((int(comb[i]) + 1)%10)
-                newCombB = "".join(newCombB)
-                newCombF = "".join(newCombF)
+                newCombB = comb[:i] + str(abs(int(comb[i]) + 9)%10) + comb[i+1:]
+                newCombF = comb[:i] + str((int(comb[i]) + 1)%10) + comb[i+1:]
+
                 if newCombB not in seen:
+                    seen.add(newCombB)
                     que.append([newCombB, count+1])
                 if newCombF not in seen:
                     que.append([newCombF, count+1])
+                    seen.add(newCombF)
         return -1
